a2a0303e831a4be38d0591837fb513014eb4c037
firstsliver59 <mushrooment2009@gmail.com>
2026-09-07 21:58:59 +0700

    Copied stuff over from another project.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hello, world")
+
+// Slideshow
+
+const homeSlides = document.querySelectorAll(".homeSlide")
+
+let index = 0
+
+homeSlides[index].style.display = "block"
+
+console.log(homeSlides[1].style.display)
