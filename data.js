3a4cce963d04b8c6ec197287c5981d093388fd8d
firstsliver59 <mushrooment2009@gmail.com>
2026-09-09 21:43:25 +0700

    Added extra values for details modal

--- data.js
+++ data.js
@@ -3,20 +3,33 @@ const LOCATIONS = [
         name: "Seaside",
         category: "Visit",
         image: "./images/seascape 4.jpeg",
-        rating: 3
-    },
-    {
-        name: "McDonald's",
-        category: "Food",
-        image: "./images/food test.jpeg",
-        rating: 4
+        rating: 3,
+        description: "A beach.",
+        location: "definitely a real place.",
+        price: "It costs nothing to go here.",
+        reviews: ["probably won't have time to use these..."]
     },
     {
         name: "Hotel",
         category: "Stay",
         image: "./images/stay test.jpeg",
-        rating: 5
+        rating: 5,
+        description: "Just your average hotel.",
+        location: "This place is definitely real, and not just a stock photo. I swear.",
+        price: "Uhhh.. $50 a night I guess.",
+        reviews: ["probably won't have time to use these..."]
+    },
+    {
+        name: "McDonald's",
+        category: "Food",
+        image: "./images/food test.jpeg",
+        rating: 4,
+        description: "The fast-food restaurant.",
+        location: "Mars.",
+        price: "More every day. It's 'inflation' trust",
+        reviews: ["probably won't have time to use these..."]
     }
+    
 ]
 
 export {
