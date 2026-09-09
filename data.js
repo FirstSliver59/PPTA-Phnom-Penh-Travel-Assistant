3c15e076e4742b302e3d6ce35dc9f16b8cc664b9
firstsliver59 <mushrooment2009@gmail.com>
2026-09-09 09:40:31 +0700

    added more test cards

--- data.js
+++ data.js
@@ -1,9 +1,21 @@
 const LOCATIONS = [
     {
         name: "Seaside",
-        category: "category",
+        category: "Visit",
         image: "./images/seascape 4.jpeg",
         rating: 3
+    },
+    {
+        name: "McDonald's",
+        category: "Food",
+        image: "./images/food test.jpeg",
+        rating: 4
+    },
+    {
+        name: "Hotel",
+        category: "Stay",
+        image: "./images/stay test.jpeg",
+        rating: 5
     }
 ]
 
